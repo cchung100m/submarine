3b1791a3526ed3affd430f9bb590c206f3957b37
cchung100m <cchung100m@cs.ccu.edu.tw>
2020-02-13 23:46:45 +0800

    Fix misconfiguration of karma.conf.js

--- submarine-workbench/workbench-web-ng/karma.conf.js
+++ submarine-workbench/workbench-web-ng/karma.conf.js
@@ -46,18 +46,16 @@ module.exports = function (config) {
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['Chrome', 'Firefox'],
-    customLaunchers: [{
+    customLaunchers: {
       ChromeHeadlessCI: {
         base: 'ChromeHeadless',
         flags: ['--no-sandbox']
-      }
-    },
-    {
+      },
       FirefoxHeadlessCI: {
         base: 'Firefox',
         flags: [ '-headless' ],        
       }
-    }],
+    },
     singleRun: false,
     restartOnFileChange: true
   });
