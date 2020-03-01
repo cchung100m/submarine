41978ad7ea4ebda27efb318a25c7a15f1e27494c
cchung100m <cchung100m@cs.ccu.edu.tw>
2020-03-01 14:04:03 +0800

    test

--- submarine-workbench/workbench-web-ng/e2e/src/app.e2e-spec.ts
+++ submarine-workbench/workbench-web-ng/e2e/src/app.e2e-spec.ts
@@ -33,12 +33,14 @@ describe('workspace-project App', () => {
   });
 
   afterEach(async () => {
-    // Assert that there are no errors emitted from the browser
-    if (browser.driver.get.toString() == "chrome") {
+    try {
+      // Assert that there are no errors emitted from the browser
       const logs = await browser.manage().logs().get(logging.Type.BROWSER);
       expect(logs).not.toContain(jasmine.objectContaining({
         level: logging.Level.SEVERE,
       } as logging.Entry)); 
+    } catch(err) {
+      console.error(err)
     }
   });
 });
